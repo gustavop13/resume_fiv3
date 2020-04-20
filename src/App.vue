cc544f088bfa909af9254a25f5faefec6fde772d
gustavop13 <gustavop@email.arizona.edu>
2020-04-20 15:29:24 -0700

    cont

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <F0ur/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import F0ur from './components/F0ur.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    F0ur
   }
 }
 </script>
 
+//13223f
+//b60025
+//cdd7ac
 <style>
+
+body, html {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  background-color: #13223f;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #b60025;
   margin-top: 60px;
+  font-size: 32pt;
 }
 </style>
