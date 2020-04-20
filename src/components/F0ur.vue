cafc11007dda60c78fc5fe377ee07fd8943d3d8d
gustavop13 <gustavop@email.arizona.edu>
2020-04-20 15:30:38 -0700

    cont2

---
+++ src/components/F0ur.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    Skills
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'F0ur',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
